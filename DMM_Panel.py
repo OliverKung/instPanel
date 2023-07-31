b382c365bf112723bf9061c3e93f4c73a73543d2
OliverKung <43801635+OliverKung@users.noreply.github.com>
2023-07-31 22:01:09 +0800

    add basic function

--- DMM_Panel.py
+++ DMM_Panel.py
@@ -1,25 +1,101 @@
 # pyBode GUI with Kivy
 import numpy as np
 import pyqtgraph as pg
-import sys
+import sys,threading
 import os,time,multiprocessing,subprocess,re
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow,QMessageBox,QSystemTrayIcon,QMenu
+from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow,QMessageBox,QSystemTrayIcon,QMenu,QLCDNumber
 from PyQt5.QtCore import QUrl,pyqtSlot,QTimer
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import PyQt5.QtCore as QtCore
 from PyQt5.QtGui import QIcon
+import threading
+from apscheduler.schedulers.blocking import BlockingScheduler
 import DMM_Panel_GUI
-import ctypes
+import UniversalDMM
+from qt_material import apply_stylesheet
 
 class mainCode(QMainWindow,DMM_Panel_GUI.Ui_MainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         DMM_Panel_GUI.Ui_MainWindow.__init__(self)
         self.setupUi(self)
+        self.STOPButton.setEnabled(False)
+        self.set_graph_ui()
+        self.bind_singnal_and_slot()
+        self.refresh_model()
+        self.time_array=np.zeros(0,dtype=float)
+        self.value_array = np.zeros(0,dtype=float)
+    
+    def bind_singnal_and_slot(self):
+        self.Model_Refresh.clicked.connect(self.refresh_model)
+        return
+    
+    def updateLCD(self):
+        with open(".\\tmp\\currentvalue.csv","r") as f:
+            self.DisplayLabel.setText(f.readline().replace("\r","").replace("\n",""))
+            f.close()
+    
+    def refresh_model(self):
+        folder_path=".\\model\\"
+        for file in os.listdir(folder_path):
+            if(file.endswith(".ini")):
+                self.DMM_Model.addItem(file.split(".ini")[0])
+    
+    @pyqtSlot()
+    def on_ConnectButton_clicked(self):
+        self.dmm = UniversalDMM.DMM(self.IP_ADDR.text(),self.DMM_Model.currentText())
+    
+    def set_graph_ui(self):
+        pg.setConfigOptions(antialias = True)
+        win = pg.GraphicsLayoutWidget()
+        self.WaveFormLayout.addWidget(win)
+        self.waveform = win.addPlot()
+
+    @pyqtSlot()
+    def on_DCV_Button_clicked(self):
+        self.on_STOPButton_clicked()
+        if(self.dmm is not None):
+            self.dmm.set_DCV()
+        self.on_StartButton_clicked()
+    
+    @pyqtSlot()
+    def on_ACV_Button_clicked(self):
+        self.on_STOPButton_clicked()
+        if(self.dmm is not None):
+            self.dmm.set_ACV()
+        self.on_StartButton_clicked()
+
+    @pyqtSlot()
+    def on_StartButton_clicked(self):
+        self.thread = threading.Thread(target=self.start_jobs)
+        self.thread.start()
+        self.STOPButton.setEnabled(True)
+        self.StartButton.setEnabled(False)
+        self.time_array=np.zeros(0,dtype=float)
+        self.value_array = np.zeros(0,dtype=float)
+
+    def start_jobs(self):
+        self.scheduler = BlockingScheduler()
+        self.scheduler.add_job(func=self.readValue,trigger="interval",seconds = 1,id = "readvaluejob")
+        self.scheduler.start()
+
+    def readValue(self):
+        value = self.dmm.read()
+        self.DisplayLabel.setText(str(value))
+        self.value_array=np.append(self.value_array,value)
+        self.time_array=np.append(self.time_array,len(self.time_array))
+        self.waveform.plot(self.time_array,self.value_array)
+
+    
+    @pyqtSlot()
+    def on_STOPButton_clicked(self):
+        self.scheduler.shutdown()
+        self.StartButton.setEnabled(True)
+        self.STOPButton.setEnabled(False)
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
-    # app.setWindowIcon(icon)
+    apply_stylesheet(app,theme="dark_amber.xml")
     mainc=mainCode()
     mainc.show()
     sys.exit(app.exec_())
---
+++ UniversalDMM.py
@@ -0,0 +1,147 @@
+# Digitam Multimeter Driver for VXI instrument. Basically write for DMM6500/7510 Series. and 3446X Serise
+import vxi11
+from enum import Enum
+
+class function(Enum):
+    DVC = 0
+    ACV = 1
+    Ohm2W = 2
+    Ohm4W = 3
+    DCI = 4
+    ACI = 5
+    FREQ = 6
+    PERIOD = 7
+    DIODE = 8
+    DCRAT = 9
+    TEMP = 10
+    CAP = 11
+
+class function_cmd_class():
+    def __init__(self,setting_str:str):
+        self.func_code = int(setting_str.split("=")[0])
+        commande_list = setting_str.split("=")[1].split(",")
+        self.func_name = commande_list[0]
+        self.func_cmd = commande_list[1]
+        self.func_enable = commande_list[2]
+        if(self.func_code != 8):
+            self.getRange = commande_list[3]
+            self.getAutoRange = commande_list[4]
+            self.getAutoZero = commande_list[5]
+            self.getNPLC = commande_list[6]
+            self.getDigits = commande_list[7]
+            self.getInputImp = commande_list[8]
+            self.setNull = commande_list[9]
+            self.RangeList = []
+            for cmd in commande_list[10:]:
+                if cmd.isdigit() == True:
+                    self.RangeList.append(cmd)
+
+class cmd_class():
+    def __init__(self,setting_str:str):
+        self.cmd_name = setting_str.split("=")[0]
+        self.cmd_value = setting_str.split("=")[1]
+
+class DMM:
+
+    def __init__(self,addr,model):
+        self.addr=addr
+        self.model=model
+        self.ini_file_path = ".\\model\\"+model+".ini"
+        self.func_list:function_cmd_class = []
+        self.cmd_list:cmd_class = []
+        self.loadCommand()
+        self.instr=vxi11.Instrument(self.addr)
+        self.instr.write("SYST:BEEP")
+        print(self.instr.ask("*IDN?"))
+    def loadCommand(self):
+        with open(self.ini_file_path,"r") as f:
+            lines = f.readlines()
+            f.close()
+            for line in lines:
+                if(line.startswith(";")==False and line.startswith("[")==False and line.isspace()==False):
+                    cmd = line.replace("\r","").replace("\n","")
+                    prefix = cmd.split("=")[0]
+                    if(prefix.isdigit()==True):
+                        self.func_list.append(function_cmd_class(cmd))
+                    else:
+                        self.cmd_list.append(cmd_class(cmd))
+    def set_DCV(self):
+        for function in self.func_list:
+            if(function.func_name == "DCV" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_ACV(self):
+        for function in self.func_list:
+            if(function.func_name == "ACV" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_Ohm2W(self):
+        for function in self.func_list:
+            if(function.func_name == "Ω 2W" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+    def set_Ohm4W(self):
+        for function in self.func_list:
+            if(function.func_name == "Ω 4W" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+            
+    def set_DCI(self):
+        for function in self.func_list:
+            if(function.func_name == "DCI" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_ACI(self):
+        for function in self.func_list:
+            if(function.func_name == "ACI" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_FREQ(self):
+        for function in self.func_list:
+            if(function.func_name == "FREQ" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_PERIOD(self):
+        for function in self.func_list:
+            if(function.func_name == "PERIOD" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_DCRAT(self):
+        for function in self.func_list:
+            if(function.func_name == "DC:RAT" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_TEMP(self):
+        for function in self.func_list:
+            if(function.func_name == "TEMP" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_CAP(self):
+        for function in self.func_list:
+            if(function.func_name == "CAP" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+
+    def set_DIODE(self):
+        for function in self.func_list:
+            if(function.func_name == "DIODE" and function.func_enable == "1"):
+                self.instr.write(function.func_cmd)
+                return
+    def read(self):
+        for cmd in self.cmd_list:
+            if(cmd.cmd_name.lower()=="read"):
+                value = self.instr.ask(cmd.cmd_value)
+        return float(value)
+
+if __name__=="__main__":
+    my_dmm=DMM("192.168.31.237","34465A")
+    my_dmm.set_DCV()
+    print(my_dmm.read())
